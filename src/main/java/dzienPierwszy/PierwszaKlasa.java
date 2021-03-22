2c6972beb5a384fa0ef95fafc9db7e982815119c
SurveyBook3 <surveybook3@gmail.com>
2021-03-22 17:44:50 +0100

    moj pierwszy commit

--- src/main/java/dzienPierwszy/PierwszaKlasa.java
+++ src/main/java/dzienPierwszy/PierwszaKlasa.java
@@ -5,6 +5,10 @@ public class PierwszaKlasa {
     public static void main(String[] args) {
 
         System.out.println("Hej ho");
+
+        int a = 10+6;
+
+        System.out.println(a);
     }
 
 }
