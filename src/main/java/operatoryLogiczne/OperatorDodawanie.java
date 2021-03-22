305b6f53ead3b6a96eea86b061e414512b444b1e
SurveyBook3 <surveybook3@gmail.com>
2021-03-22 18:01:58 +0100

    Operatory logiczne - dzien pierwszy

---
+++ src/main/java/operatoryLogiczne/OperatorDodawanie.java
@@ -0,0 +1,20 @@
+package operatoryLogiczne;
+
+public class OperatorDodawanie {
+
+
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("DUPA");
+
+        int a = 5;
+        int b = 10;
+
+        // tekst, który ma być wyświetlony dajesz w ciapkach -> ""
+        System.out.println("Czy a jes mniejsze od b? Odp = " + (a<b));
+        System.out.println(a>b);
+        System.out.println(a==b);
+    }
+
+}
